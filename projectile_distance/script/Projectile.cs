77a0121300c62cda036886e9b0b016a5c60b50cf
Mario Laframboise <laframboise.mario@gmail.com>
2026-03-11 17:40:18 -0400

    add : projectile tween

--- projectile_distance/script/Projectile.cs
+++ projectile_distance/script/Projectile.cs
@@ -17,6 +17,23 @@ public partial class Projectile : Node2D, ICiblable
     [Export]
     Poursuite Poursuite;
 
+    public override void _Ready() {
+        base._Ready();
+
+        Color color = Modulate;
+        color.A = 0.0f;
+        Modulate = color;
+
+        Tween tween = CreateTween();
+        
+        tween.SetEase(Tween.EaseType.Out).SetTrans(Tween.TransitionType.Circ);
+
+        tween.TweenProperty(this, "modulate:a", 1.0f, 0.3f);
+        tween.SetParallel();
+        tween.TweenProperty(this, "scale:y", 1.0f, 0.3f).From(1.5f);
+
+    }
+
     public void SetCible(Node2D InCible)
     {
         Cible = (Node2D)InCible;
