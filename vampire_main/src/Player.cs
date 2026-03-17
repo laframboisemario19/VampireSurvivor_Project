6f17e8f516a7d6f46d0655f40cc73e2f0f5a9e8e
laframboisemario19 <laframboise.mario@gmail.com>
2026-03-17 16:01:38 -0400

    add : animation de direction du player

--- vampire_main/src/Player.cs
+++ vampire_main/src/Player.cs
@@ -6,6 +6,33 @@ public partial class Player : Node2D
 
 	[Export] private TextureProgressBar t;
 
+	[Export]
+	AnimatedSprite2D animatedPlayer;
+	public override void _Process(double delta)
+	{
+		if(Input.IsActionPressed("ui_right"))
+		{
+			animatedPlayer.Play("marche_droite");
+		}
+		else if (Input.IsActionPressed("ui_left"))
+		{
+			animatedPlayer.Play("marche_gauche");
+		}
+		else if (Input.IsActionPressed("ui_up"))
+		{
+			animatedPlayer.Play("marche_haut");
+		}
+		else if (Input.IsActionPressed("ui_down"))
+		{
+			animatedPlayer.Play("marche_bas");
+		}
+		else
+		{
+			animatedPlayer.Play("marche_bas");
+			animatedPlayer.Stop();
+		}
+	}
+
 	public void AddXp(int value)
 	{
 		t.Value += value;
