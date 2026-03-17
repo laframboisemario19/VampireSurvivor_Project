7a4d48f15eaba5ac6c2409865e92ba14fd82a401
laframboisemario19 <laframboise.mario@gmail.com>
2026-03-17 14:04:11 -0400

    add : version commune -> mash-up environnement + projectile_distance

---
+++ vampire_main/src/MedCible.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Godot;
+using Utils;
+
+public partial class MedCible : Node2D
+{
+    [ExportGroup("External")]
+    [Export]
+    private DcmProjectile SpawnerProjectile; 
+    [Export]
+    private DcmEnemyTest SpawnerEnemy;
+    [Export]
+    private PlayerTest Player;
+
+    public enum EAlgoSelectionCible
+    {
+        eClosestEnemy,
+        ePlayer
+    }
+
+    public Node2D ChoisirCible(EAlgoSelectionCible InAlgoSelectionCible, Vector2 InPosition)
+    {
+        Node2D ret = null;
+        switch (InAlgoSelectionCible)
+        {
+            default:
+            case EAlgoSelectionCible.ePlayer:
+                {
+                    // à coder
+                    ret = null;
+                }
+                break;
+            case EAlgoSelectionCible.eClosestEnemy:
+                {
+                    IEnumerable<EnemyTest> allEnemies = SpawnerEnemy.EnsureValid().GatherChildren().OfType<EnemyTest>();
+                    ret = allEnemies.First();
+                    float currentength = (InPosition - ret.GlobalPosition).Length();
+                    foreach (EnemyTest enemy in allEnemies)
+                    {
+                        float length = (InPosition - enemy.GlobalPosition).Length();
+                    
+                        if (length < currentength)
+                        {
+                            ret = enemy;
+                            currentength = length;
+                        }
+                    }
+                }
+                break;
+        }
+        return ret;
+    }
+
+    public Vector2 GetPlayerPosition()
+    {
+        return Player.EnsureValid().GlobalPosition;
+    }
+}
