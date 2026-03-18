594e96a084ccf763f489be85f320cb95774da5c3
jonathan <jonathan.barahona.g@outlook.com>
2026-03-18 12:27:22 -0400

    fix : spawn 2.0

--- vampire_main/src/BaseEnemy.cs
+++ vampire_main/src/BaseEnemy.cs
@@ -14,6 +14,37 @@ public partial class BaseEnemy : Node2D
     protected AnimatedSprite2D AnimatedSprite;
     public bool isDying = false;
 
+    // public void TakeDamage()
+    // {
+    //     if (!isDying)
+    //     {
+    //         isDying = true;
+    //         var anim = Area.GetParent().GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+    //         string currentAnimName = anim.Animation;
+
+    //         switch (currentAnimName)
+    //         {
+    //             case "marche_bas":
+    //                 v = new((float)GD.RandRange(-_tailleX, _tailleX), -_decalageY);
+    //                 break;
+    //             case "marche_haut":
+    //                 v = new(_decalageX, (float)GD.RandRange(-_decalageY, _decalageY));
+    //                 break;
+    //             case "marche_cote":
+    //                 v = new((float)GD.RandRange(-_tailleX, _tailleX), _decalageY);
+    //                 break;
+    //             case 3:
+    //                 v = new(_decalageX, (float)GD.RandRange(-_decalageY, _decalageY));
+    //                 break;
+    //         }
+
+    //         Area.GlobalPosition.Area.Die();
+    //         Poursuite.Velocity = 60.0f;
+    //         AnimatedSprite.Play("explode");
+    //         AnimatedSprite.AnimationFinished += () => QueueFree();
+    //     }
+    // }
+
     public void Die()
     {
         if (!isDying)
--- vampire_main/src/DcmEnemi.cs
+++ vampire_main/src/DcmEnemi.cs
@@ -26,18 +26,34 @@ public partial class DcmEnemi : Node2D, ISpawn
     [Export]
     private Timer timer;
 
-    private Vector2 _offsetBounds = new(1920, 1080);
-    private float _tailleX = 1000.0f;
-    private float _tailleY = 600.0f;
-    private float _decalageX = 900.0f;
-    private float _decalageY = 500.0f;
+    private float _tailleX = 700.0f;
+    private float _tailleY = 500.0f;
+
+    // private float _decalageX = 900.0f;
+    // private float _decalageY = 500.0f;
+    private float _buffer = 10.0f;
 
     public override void _Ready()
     {
         base._Ready();
         timer.EnsureValid().Timeout += () =>
         {
+            Camera2D _camera2D = MediateurCible.GetParent().GetNode<Camera2D>("Camera2D");
+            GD.Print(GetViewport().GetVisibleRect().Size.Y);
             var spawn = (Node2D)((ISpawn)this).Spawn(SpawneeScene);
+
+            Vector2 playerPos = MediateurCible.GetPlayerPosition();
+
+            Vector2 cameraSize = _camera2D.GetViewportRect().Size / _camera2D.Zoom;
+
+            float _cameraH = cameraSize.Y;
+
+            float _cameraW = cameraSize.X;
+
+            float cameraTop = playerPos.Y - (_cameraH / 2);
+            float cameraBottom = playerPos.Y + (_cameraH / 2);
+            float cameraRight = playerPos.X + (_cameraW / 2);
+            float cameraLeft = playerPos.X - (_cameraW / 2);
 
             int coteApparition = GD.RandRange(0, 3);
             Vector2 v = new(0, 0);
@@ -45,20 +61,49 @@ public partial class DcmEnemi : Node2D, ISpawn
             switch (coteApparition)
             {
                 case 0:
-                    v = new((float)GD.RandRange(-_tailleX, _tailleX), -_decalageY);
-                    break;
+                    if (cameraTop - _buffer >= -_tailleY / 2)
+                    {
+                        v = new(
+                            (float)GD.RandRange(-_tailleX / 2, _tailleX / 2),
+                            (float)GD.RandRange(-_cameraH / 2 - _buffer, -_tailleY / 2)
+                        );
+                        break;
+                    }
+                    goto case 2;
                 case 1:
-                    v = new(_decalageX, (float)GD.RandRange(-_decalageY, _decalageY));
-                    break;
+                    if (cameraRight + _buffer <= _tailleX / 2)
+                    {
+                        v = new(
+                            (float)GD.RandRange(_cameraW / 2 + _buffer, _tailleX / 2),
+                            (float)GD.RandRange(_tailleY / 2, -_tailleY / 2)
+                        );
+                        break;
+                    }
+                    goto case 3;
                 case 2:
-                    v = new((float)GD.RandRange(-_tailleX, _tailleX), _decalageY);
-                    break;
+                    if (cameraBottom + _buffer <= _tailleY / 2)
+                    {
+                        v = new(
+                            (float)GD.RandRange(-_tailleX / 2, _tailleX / 2),
+                            (float)GD.RandRange(_cameraH / 2 + _buffer, _tailleY / 2)
+                        );
+                        break;
+                    }
+                    goto case 0;
                 case 3:
-                    v = new(_decalageX, (float)GD.RandRange(-_decalageY, _decalageY));
-                    break;
+                    if (cameraLeft - _buffer >= -_tailleX / 2)
+                    {
+                        v = new(
+                            (float)GD.RandRange(-_cameraW / 2 + _buffer, -_tailleX / 2),
+                            (float)GD.RandRange(_tailleY / 2, -_tailleY / 2)
+                        );
+                        break;
+                    }
+                    goto case 1;
             }
 
-            spawn.GlobalPosition = GlobalPosition + v;
+            //spawn.GlobalPosition = MediateurCible.GetPlayerPosition() + v;
+            spawn.GlobalPosition += v;
 
             Node2D cible = MediateurCible
                 .EnsureValid()
